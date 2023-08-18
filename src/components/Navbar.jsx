f0e1deecd8f8ff7256340a35cd3c3508531cb12e
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-18 22:16:53 +0900

    feat: admin account implement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,10 @@ export default function Navbar() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    onUserStateChage(setUser);
+    onUserStateChage(user => {
+      console.log(user);
+      setUser(user);
+    });
   }, []);
 
   const handleLogin = () => {
