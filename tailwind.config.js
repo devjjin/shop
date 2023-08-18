f089b74e56f9bf655e8804e14d538416f40e07ac
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-18 16:11:38 +0900

    design: Navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#F96162',
+      },
+    },
   },
   plugins: [],
 };
