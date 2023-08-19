8a06c852a8924c47a6dde46b2d072c214561c1cc
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-19 10:51:52 +0900

    feat: page access restriction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { AUthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <>
+    <AUthContextProvider>
       <Navbar />
       <Outlet />
-    </>
+    </AUthContextProvider>
   );
 }
 
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { login, logout, onUserStateChage } from '../api/firebase';
+
+const AuthContext = createContext();
+
+export function AUthContextProvider({ children }) {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChage(user => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
