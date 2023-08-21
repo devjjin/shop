93c7abf0f757d0bc278ee8ae470261aa8b47ab51
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-21 18:29:03 +0900

    feat: new product image add cloudinary implement

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import Button from '../components/ui/Button';
+import { uploadImage } from '../api/uploader';
 
 export default function NewProduct() {
   const [product, setProduct] = useState();
   const [file, setFile] = useState();
 
-  const handleSubmit = e => {};
+  const handleSubmit = e => {
+    e.preventDefault();
+    uploadImage(file).then(url => {
+      console.log(url);
+    });
+  };
 
   const handleChage = e => {
     const { name, value, files } = e.target;
@@ -19,55 +25,56 @@ export default function NewProduct() {
   return (
     <section>
       {file && <img src={URL.createObjectURL(file)} alt="local file" />}
-      <form onSubmit={handleSubmit}></form>
-      <input
-        type="file"
-        accept="image/*"
-        name="file"
-        required
-        onChange={handleChage}
-      />
-      <input
-        type="text"
-        name="title"
-        value={product.title ?? ''}
-        placeholder="제품명"
-        required
-        onChange={handleChage}
-      />
-      <input
-        type="number"
-        name="price"
-        value={product.price ?? ''}
-        placeholder="가격"
-        required
-        onChange={handleChage}
-      />
-      <input
-        type="text"
-        name="category"
-        value={product.category ?? ''}
-        placeholder="카테고리"
-        required
-        onChange={handleChage}
-      />
-      <input
-        type="text"
-        name="description"
-        value={product.name ?? ''}
-        placeholder="제품 설명"
-        required
-        onChange={handleChage}
-      />
-      <input
-        type="text"
-        name="options"
-        value={product.options ?? ''}
-        placeholder="옵션들(콤마(,)로 구분)"
-        required
-        onChange={handleChage}
-      />
-      <Button text={'제품 등록하기'} />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          required
+          onChange={handleChage}
+        />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ''}
+          placeholder="제품명"
+          required
+          onChange={handleChage}
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? ''}
+          placeholder="가격"
+          required
+          onChange={handleChage}
+        />
+        <input
+          type="text"
+          name="category"
+          value={product.category ?? ''}
+          placeholder="카테고리"
+          required
+          onChange={handleChage}
+        />
+        <input
+          type="text"
+          name="description"
+          value={product.name ?? ''}
+          placeholder="제품 설명"
+          required
+          onChange={handleChage}
+        />
+        <input
+          type="text"
+          name="options"
+          value={product.options ?? ''}
+          placeholder="옵션들(콤마(,)로 구분)"
+          required
+          onChange={handleChage}
+        />
+        <Button text={'제품 등록하기'} />
+      </form>
     </section>
   );
 }
