92c68ea4bde604e8a94b7491dcf3a3b55a389ed3
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-22 10:15:02 +0900

    fix: missing return statement in uploadImage

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -2,7 +2,7 @@ export async function uploadImage(file) {
   const data = new FormData();
   data.append('file', file);
   data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
-  fetch(process.env.REACT_APP_CLOUDINARY_URL, {
+  return fetch(process.env.REACT_APP_CLOUDINARY_URL, {
     method: 'POST',
     body: data,
   })
