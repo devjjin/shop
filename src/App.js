8590c5501b7bf49495ac3fd2d1f1da567d562aac
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-22 11:38:21 +0900

    chore: queryclient setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AUthContextProvider } from './context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <AUthContextProvider>
-      <Navbar />
-      <Outlet />
-    </AUthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AUthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AUthContextProvider>
+    </QueryClientProvider>
   );
 }
 
