310ad692550a2dcb1b53d3fbe7ebbf2a81610f82
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-23 11:27:25 +0900

    refactor: new product with useMutation

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from '../components/ui/Button';
 import { uploadImage } from '../api/uploader';
 import { addNewProduct } from '../api/firebase';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
@@ -9,18 +10,32 @@ export default function NewProduct() {
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
 
+  const queryClient = useQueryClient();
+  const addProduct = useMutation(
+    ({ product, url }) => addNewProduct(product, url),
+    {
+      onSucces: () => {
+        queryClient.invalidateQueries(['products']);
+      },
+    },
+  );
+
   const handleSubmit = e => {
     e.preventDefault();
     setIsUploading(true);
     uploadImage(file) //
       .then(url => {
-        addNewProduct(product, url) //
-          .then(() => {
-            setSuccess('성공적으로 제품이 추가되었습니다.');
-            setTimeout(() => {
-              setSuccess(null);
-            }, 4000);
-          });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSucces: () => {
+              setSuccess('성공적으로 제품이 추가되었습니다.');
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          },
+        );
       })
       .finally(() => setIsUploading(false));
   };
